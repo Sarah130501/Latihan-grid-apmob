7f06ee9edba53875985e075b04dff62b7315550f
unknown <maisarah9991620@gmail.com>
2023-10-15 03:48:25 -0700

    Latihan

---
+++ grid.page.spec.ts
@@ -0,0 +1,19 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { GridPage } from './grid.page';
+import { IonIcon } from '@ionic/angular';
+import { ToastController } from '@ionic/angular';
+
+describe('GridPage', () => {
+  let component: GridPage;
+  let fixture: ComponentFixture<GridPage>;
+
+  beforeEach(async(() => {
+    fixture = TestBed.createComponent(GridPage);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  }));
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
